ae8000eb559c796405abbc7bfa79e7c9adc933ba
PDx3 <dragger4000@hotmail.com>
2017-02-12 16:28:29 +0100

    demo

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,11 +12,19 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/index')
 });
 
-app.listen(app.get('port'), function() {
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+     io.emit('chat message', msg);
+  });
+});
+
+http.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
