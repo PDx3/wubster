411eb46de2545e90ded253b2ffaaccc0bc576bce
PDx3 <dragger4000@hotmail.com>
2017-02-12 17:33:23 +0100

    demo

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index')
+  response.render('pages/index2')
 });
 
 app.get('/cool', function(request, response) {
